d0fbeb0a7fe43d732bebc6f528cb7d7647c717d8
guilhermemacielvieira-a11y <guilherme.maciel.vieira@gmail.com>
2026-02-12 11:54:10 -0300

    fix: adicionar useState no import de EnviosExpedicaoPage (crash)

--- src/pages/EnviosExpedicaoPage.jsx
+++ src/pages/EnviosExpedicaoPage.jsx
@@ -1,4 +1,4 @@
-import React, { useMemo } from 'react';
+import React, { useState, useMemo } from 'react';
 import { motion } from 'framer-motion';
 import toast from 'react-hot-toast';
 import { exportToExcel } from '@/utils/exportUtils';
